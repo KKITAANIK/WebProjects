f0f81cfb2f13a3240ac13c00d1e9ead8b71afeae
KK̈ITAȺNIK <kkitaanik@gmail.com>
2020-09-17 15:29:45 -0700

    implimenting calculator

--- GST/calc.js
+++ GST/calc.js
@@ -5,6 +5,8 @@ let day;
 let month;
 let year;
 
+let date;
+
 document.getElementById("button").onclick = function(){
     sec = ((Date.parse(document.getElementById("dateInput").value) - 1597536000000) * 0.9072) / 1000;
     min = 39;
@@ -12,106 +14,111 @@ document.getElementById("button").onclick = function(){
     day = 6;
     month = 2;
     year = 6896;
-    if (sec >= 0) {
-        while (sec >= 100) {
-            sec -= 100;
-            min++;
-        }
-        while (min >= 50) {
-            min -= 50;
-            hour++;
-        }
-        while (hour >= 25) {
-            hour -= 25;
-            day++;
-        }
-        while ((day > 35) || (day > 34 && ((month % 2 == 0) || (month == 1 && (year % 4 == 0))))) {
-            if ((month % 2 == 1) && (year % 4 != 0)) {
-                day -= 35;
-                month++;
-            }
-            else {
-                day -= 34;
-                month++;
-            }
-            
-            if (month > 9) {
-                month -= 9;
-                year++;
-            }
-        }
+
+    date = new Date(document.getElementById("dateInput").value);
+
+    if (date == "Invalid Date") {
+        document.getElementById("display").innerHTML = "invalid Date.";
+        document.getElementById("toptext").innerHTML = "";
     }
     else {
-        while (sec < 0) {
-            sec += 100;
-            min--;
-        }
-        while (min < 0) {
-            min += 50;
-            hour--;
-        }
-        while (hour < 0) {
-            hour += 25;
-            day--;
-        }
-        while (day <= 0) {
-            if ((month % 2 == 1) || ((year % 4 == 0) && month == 2)) {
-                day += 34;
-                month--;
+        if (sec >= 0) {
+            while (sec >= 100) {
+                sec -= 100;
+                min++;
             }
-            else {
-                day += 35;
-                month--;
+            while (min >= 50) {
+                min -= 50;
+                hour++;
             }
-            
-            if (month <= 0) {
-                month += 9;
-                year--;
+            while (hour >= 25) {
+                hour -= 25;
+                day++;
             }
-        }
-    }
-
-    if (year >= 0) {
-        if (hour < 10) {
-            if (min < 10) {
-                document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
-            }
-            else {
-                document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+            while ((day > 35) || (day > 34 && ((month % 2 == 0) || (month == 1 && (year % 4 == 0))))) {
+                if ((month % 2 == 1) && (year % 4 != 0)) {
+                    day -= 35;
+                    month++;
+                }
+                else {
+                    day -= 34;
+                    month++;
+                }
+                
+                if (month > 9) {
+                    month -= 9;
+                    year++;
+                }
             }
         }
         else {
-            if (min < 10) {
-                document.getElementById("display").innerHTML = hour.toString().concat(":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+            while (sec < 0) {
+                sec += 100;
+                min--;
             }
-            else {
-                document.getElementById("display").innerHTML = hour.toString().concat(":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+            while (min < 0) {
+                min += 50;
+                hour--;
+            }
+            while (hour < 0) {
+                hour += 25;
+                day--;
+            }
+            while (day <= 0) {
+                if ((month % 2 == 1) || ((year % 4 == 0) && month == 2)) {
+                    day += 34;
+                    month--;
+                }
+                else {
+                    day += 35;
+                    month--;
+                }
+                
+                if (month <= 0) {
+                    month += 9;
+                    year--;
+                }
             }
         }
-    }
-    else {
-        if (hour < 10) {
-            if (min < 10) {
-                document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+
+        date = date.toString().replace(/I/g, 'i');
+        document.getElementById("toptext").innerHTML = date.concat(" in GST is*")
+
+        if (year >= 0) {
+            if (hour < 10) {
+                if (min < 10) {
+                    document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+                }
+                else {
+                    document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+                }
             }
             else {
-                document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                if (min < 10) {
+                    document.getElementById("display").innerHTML = hour.toString().concat(":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+                }
+                else {
+                    document.getElementById("display").innerHTML = hour.toString().concat(":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " AA GST");
+                }
             }
         }
         else {
-            if (min < 10) {
-                document.getElementById("display").innerHTML = hour.toString().concat(":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+            if (hour < 10) {
+                if (min < 10) {
+                    document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                }
+                else {
+                    document.getElementById("display").innerHTML = "0".concat(hour.toString(), ":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                }
             }
             else {
-                document.getElementById("display").innerHTML = hour.toString().concat(":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                if (min < 10) {
+                    document.getElementById("display").innerHTML = hour.toString().concat(":0", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                }
+                else {
+                    document.getElementById("display").innerHTML = hour.toString().concat(":", min.toString(), " ", day.toString(), "/", month.toString(), "/", year.toString(), " BA GST");
+                }
             }
         }
     }
-
-    console.log(sec);
-    console.log(min);
-    console.log(hour);
-    console.log(day);
-    console.log(month);
-    console.log(year);
 };
