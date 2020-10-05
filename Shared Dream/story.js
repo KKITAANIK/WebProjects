1fc623664c1cb0d7a42a87f66cc2b9fa6d0924e3
KK̈ITAȺNIK <kkitaanik@gmail.com>
2020-10-05 16:26:33 -0700

    Update story.js

--- Shared Dream/story.js
+++ Shared Dream/story.js
@@ -1,7 +1,7 @@
 //subject, object, pronominal adjective, predicative adjective, reflexive, contraction
 
 function Awaken() {
-    Output("&emsp;&emsp;&emsp;&emsp;\"Hey! Cáel! I think.. I think ".concat(gender[4], " waking up!\""));
+    Output("&emsp;&emsp;&emsp;&emsp;\"Hey! Cáel! I think.. I think ".concat(gender[5], " waking up!\""));
     ClearButtons();
     button[0].update(PassTime, "Pass the time.");
 }
