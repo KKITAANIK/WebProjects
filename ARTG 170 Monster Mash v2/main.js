4c3dc44b3b61e886c5e7b6bd7929e4db565982eb
KK̈ITAȺNIK <pu0awesomeness0re@gmail.com>
2023-11-02 21:24:21 -0700

    Starting Monster Mash v2

---
+++ ARTG 170 Monster Mash v2/main.js
@@ -0,0 +1,72 @@
+function timer(ms) { return new Promise(res => setTimeout(res, ms)); }
+
+async function SlowType(output, startwait = 0, delay = 10, onclick = undefined) {
+    chararray = output.split("");
+    totaloutput = document.getElementById("output").innerHTML + "<p>";
+    let italicbool = false;
+    let linkbool = false;
+    if (startwait > 0) {
+        await timer(startwait);
+    }
+    for (let i = 0; i < chararray.length; i++) {
+        if (chararray[i] == '|') {
+            chararray[i] = "<br/>"
+        }
+        else if (chararray[i] == 'Ⅴ') {
+            chararray[i] = "";
+            await timer(500 - delay);
+        }
+        else if (chararray[i] == "*") {
+            if (italicbool == false) {
+                chararray[i] = "<i>"
+                italicbool = true;
+            }
+            else if (italicbool == true) {
+                chararray[i] = "</i>";
+                italicbool = false;
+            }
+        }
+        if (chararray[i] == '※') {
+            if (linkbool == false) {
+                if (onclick == undefined) {
+                    throw new Error("SlowType param 'onclick' not defined.")
+                }
+                else {
+                    chararray[i] = "<a onclick=\"" + onclick + "\">";
+                    linkbool = true;
+                }
+            }
+            else if (linkbool == true) {
+                chararray[i] = "</a>"
+                linkbool = false;
+            }
+        }
+        await timer(delay);
+        document.getElementById("output").innerHTML = totaloutput + chararray[i];
+        totaloutput += chararray[i];
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+}
+
+async function Play(key) {
+    let links = document.getElementsByTagName("a");
+    for(let i = 0; i < links.length; i++) {
+        if (links[i].style.color != "#c0c0c0") {
+            links[i].style.color = "#c0c0c0";
+            links[i].style.cursor = "text"
+            links[i].removeAttribute('onclick');
+        }
+    }
+    
+    if (key == 0) {
+        await SlowType("It lingers.", 2000, 50);
+        await SlowType("※Strain to see.※", 4000, 10, "Play(1);")
+    }
+    else if (key == 1) {
+        await SlowType("You strain your eyes, squinting into the deep black that surrounds you.Ⅴ No matter how strongly you peer into the darkness, no finer detail reveals itself.Ⅴ You are blind.");
+        await SlowType("※Strain to hear.※", 2000, 10, "Play(2);")
+    }
+    else if (key == 2) {
+        
+    }
+}
