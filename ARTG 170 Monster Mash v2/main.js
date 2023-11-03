4061753a3d3ed92a830d9503329f0929fdd28654
KK̈ITAȺNIK <35880991+KKITAANIK@users.noreply.github.com>
2023-11-03 15:46:23 -0700

    more work on monster mash v2

--- ARTG 170 Monster Mash v2/main.js
+++ ARTG 170 Monster Mash v2/main.js
@@ -32,7 +32,7 @@ async function SlowType(output, startwait = 0, delay = 10, onclick = undefined) 
                     throw new Error("SlowType param 'onclick' not defined.")
                 }
                 else {
-                    chararray[i] = "<a onclick=\"" + onclick + "\">";
+                    chararray[i] = "<a onclick=\"this.style.color = '#c0c0c0';this.style.cursor = 'text';this.removeAttribute('onclick');" + onclick + "\">";
                     linkbool = true;
                 }
             }
@@ -51,14 +51,18 @@ async function SlowType(output, startwait = 0, delay = 10, onclick = undefined) 
 async function Play(key) {
     let links = document.getElementsByTagName("a");
     for(let i = 0; i < links.length; i++) {
-        if (links[i].style.color != "#c0c0c0") {
-            links[i].style.color = "#c0c0c0";
+        if (links[i].style.cursor != "text") {
             links[i].style.cursor = "text"
             links[i].removeAttribute('onclick');
+            links[i].style.color = '#404040';
         }
     }
     
     if (key == 0) {
+        document.getElementById("output").innerHTML = "";
+        var audio = new Audio('Audio/monsters-cave-159887.mp3');
+        audio.loop = "true";
+        audio.play();
         await SlowType("It lingers.", 2000, 50);
         await SlowType("※Strain to see.※", 4000, 10, "Play(1);")
     }
@@ -67,8 +71,20 @@ async function Play(key) {
         await SlowType("※Strain to hear.※", 2000, 10, "Play(2);")
     }
     else if (key == 2) {
-        await SlowType("You focus.Ⅴ Somewhere, instinctually, you recognize the immense silence surrounding you.Ⅴ Your breathing is uncompromisingly audible, and when a drip of water sounds from some unknown direction, it's almost deafening.");
-        await SlowType("The sound echoes through the space, implying an immense internal volume and making it impossible to place.Ⅴ Several seconds pass, and you hear another.Ⅴ Wherever you are, it is...Ⅴ wet.", 1000)
+        await SlowType("You focus.Ⅴ Somewhere, instinctively, you recognize the dense silence that envelops your senses.Ⅴ Your breathing is uncompromisingly audible, and when a drip of water rings out from some unknown direction, it's almost deafening.");
+        await SlowType("The sound echoes through the space, impossible to locate, implying an immense internal volume.Ⅴ Several seconds pass, and you hear another.Ⅴ Wherever you are, it is...Ⅴ wet.", 1000)
         await SlowType("※Strain to feel.※", 2000, 10, "Play(3);")
+    }
+    else if (key == 3) {
+        await SlowType("You shift, working to build a sense of your body.Ⅴ You are sitting, your legs against a firm, unyielding ground, with a similarly hard surface pressing into your back.Ⅴ You lower a hand, grazing the floor and wall.Ⅴ You're met with cold roughness, slightly damp.Ⅴ The air feels dense and humid in your throat and against your skin.");
+        await SlowType("You are surrounded by stone.", 1000)
+        await SlowType("※Stand.※", 2000, 10, "Play(4);")
+        await SlowType("※Rest.※", 500, 10, "Play(5);")
+    }
+    else if (key == 4) {
+        await SlowType("You anchor your hands at your sides, slowly pushing to your feet.Ⅴ You move carefully, and it is for that that you avoid hitting your head on the low ceiling.Ⅴ You are forced to hunch slightly—head lowered, knees bent—as you find your footing.")
+    }
+    else if (key == 5) {
+        await SlowType("You close your eyes, though it changes your vision none.Ⅴ At least it allows you some sense of rest.Ⅴ You do not know where you are, but movement does not feel like the right answer.")
     }
 }
