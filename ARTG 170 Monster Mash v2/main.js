bb4b717389d722145732a8bf91616971eca946ea
KK̈ITAȺNIK <35880991+KKITAANIK@users.noreply.github.com>
2023-11-08 13:36:56 -0800

    Update main.js

--- ARTG 170 Monster Mash v2/main.js
+++ ARTG 170 Monster Mash v2/main.js
@@ -1,5 +1,8 @@
 function timer(ms) { return new Promise(res => setTimeout(res, ms)); }
 
+let p = {};
+p.wounds = 0;
+
 async function SlowType(output, startwait = 0, delay = 10, onclick = undefined) {
     chararray = output.split("");
     totaloutput = document.getElementById("output").innerHTML + "<p>";
@@ -87,22 +90,22 @@ async function Play(key) {
     }
     else if (key == 4) {
         await SlowType("You anchor your hands at your sides, slowly pushing to your feet.Ⅴ You move carefully, and it is for that that you avoid hitting your head on the low ceiling.Ⅴ You are forced to hunch slightly—head lowered, knees bent—as you find your footing.");
+        await SlowType("A rumbling, growling sound comes from your left, though you can discern no finer direction.Ⅴ The sound itself melts into the rest of the environment, giving it the same character as this place's natural background of wind and geology, though you can't shake the notion that it might be something more...Ⅴ animal.", 1000);
+        await SlowType("※Wait.※", 2000, 10, "Play(6);");
+        await SlowType("※Flee.※", 500, 10, "Play(7);");
     }
     else if (key == 5) {
         await SlowType("You close your eyes, though it changes your vision none.Ⅴ At least it allows you some sense of rest.Ⅴ You do not know where you are, but movement does not feel like the right answer.");
         await SlowType("A rumbling, growling sound from your left forces you to your feet moments later, your ascent quick enough that you knock your head† against the low ceiling.Ⅴ There's an immediate throbbing in your skull, but you gather yourself, instinctively looking for the source of the sound.", 1000);
-        await SlowType("The darkness offers you nothing.", 1000);
+        p.wounds++;
+        await SlowType("The darkness offers you nothing.Ⅴ By the time you've gathered yourself, it has already faded into the natural background of wind and geology.Ⅴ Perhaps that's the mechanism from which it came.", 1000);
         await SlowType("※Wait.※", 2000, 10, "Play(6);");
         await SlowType("※Flee.※", 500, 10, "Play(7);");
     }
     else if (key == 6) {
-        
+        await SlowType("You stand still, ears open for some new disturbance.Ⅴ You feel your ")
     }
     else if (key == 7) {
-        await SlowType("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare faucibus sem, eget auctor eros commodo ac. Nam elementum, dui eget tincidunt luctus, tortor nunc rutrum ex, vitae aliquam ante dui eu augue. Nulla vitae gravida enim. Nunc luctus et felis sit amet varius. Sed sagittis pretium tortor nec scelerisque. Aliquam erat volutpat. Curabitur id massa eu nisl pulvinar vulputate a id orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum scelerisque mollis orci sed molestie.");
-        await SlowType("Etiam aliquet, mi nec mattis blandit, tortor quam consectetur nunc, a efficitur mi orci a nunc. Sed sed viverra nisl, eget iaculis augue. Phasellus vestibulum, nisl et sollicitudin consectetur, lacus sapien sodales nisl, sed aliquam lacus orci et dui. Donec eu augue eros. Etiam mauris ante, maximus in finibus ut, ultricies sodales est. Duis luctus eros nibh, eu aliquam purus vulputate a. Fusce eget risus tincidunt, egestas purus et, consequat ante. Pellentesque hendrerit justo at magna volutpat accumsan. Morbi ullamcorper venenatis blandit. Aenean lacinia magna ex, ut condimentum purus laoreet nec. Integer quis ex auctor, scelerisque dolor vitae, placerat orci. Phasellus elementum orci non consectetur interdum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti.");
-        await SlowType("Etiam imperdiet, eros porta scelerisque posuere, eros elit consequat arcu, vitae facilisis leo nulla nec orci. Cras at augue vel risus eleifend condimentum id sed felis. In sit amet turpis quis urna lacinia elementum ut at ex. Suspendisse potenti. Sed eu libero sodales, pharetra tortor in, aliquet ipsum. Fusce a felis ut ipsum mattis lobortis. In scelerisque ligula a egestas posuere. Mauris elementum placerat aliquet. Nam fermentum a nisl in lobortis. Ut consectetur libero at dignissim feugiat.");
-        await SlowType("In tincidunt leo in eros ultricies, ut porttitor nulla malesuada. In tincidunt, ligula ut dapibus euismod, mi lacus porta nisi, ultrices viverra justo eros eu mauris. Maecenas tincidunt vulputate suscipit. Nam eget ullamcorper justo, non commodo dolor. Nullam pellentesque erat vitae elit cursus scelerisque. Ut in feugiat massa. Nullam cursus, quam ac bibendum iaculis, orci tellus pharetra dui, sit amet mollis urna tellus a orci. Proin aliquet est nec diam tincidunt blandit. Maecenas vel finibus est, et faucibus augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed faucibus erat ut ligula aliquam, et aliquam justo sollicitudin. Proin ac metus euismod, ornare mi vel, consectetur odio. Praesent dapibus arcu at tristique bibendum. Nunc nec bibendum ipsum, laoreet porttitor mi.");
-        await SlowType("Nunc enim orci, consequat a consectetur et, tempus vel purus. Maecenas eleifend lectus leo. Quisque in vestibulum sapien. Proin molestie rutrum nunc at congue. Pellentesque tincidunt lacus sed ante tincidunt, eu convallis tortor volutpat. Integer bibendum, neque quis accumsan tristique, metus turpis luctus neque, at efficitur turpis felis ornare nisl. Suspendisse et lorem viverra, cursus elit quis, interdum eros. Sed dapibus ullamcorper lacus, id posuere mi cursus sed. Morbi ornare venenatis magna, at varius diam gravida interdum. In nec ultrices metus, eget facilisis mi. Vestibulum nec lectus ex. Sed eget lacus diam.");
+        
     }
 }
