2f1886cdce46f2596ebe1b26aea0e8a5f9fce0ec
KK̈ITAȺNIK <pu0awesomeness0re@gmail.com>
2024-01-31 02:53:08 -0800

    more adjustments to continuity

--- Continuity/main.js
+++ Continuity/main.js
@@ -35,22 +35,22 @@ function Main(key, param, subparam) {
 	document.getElementById("duNumber").value = 50;
 	document.getElementById("otherNumber").value = 50;
 	if (key == 0) {
-		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, while another instance of you instantaneously appears in the output. What is the correct interpretation?</p>`);
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, while <b>another instance of you</b> instantaneously appears in the output. What is the correct interpretation?</p>`);
 		document.getElementById("sliders").style.display = "block";
 		document.getElementById("submitButton").innerHTML = "Submit.";
 		SetSubmit(Main.bind(null, 1))
 	}
 	else if (key == 1) {
-		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, while another instance of you continues to exist in the input. What is the correct interpretation?</p>`);
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, while <b>another instance of you</b> continues to exist in the input. What is the correct interpretation?</p>`);
 		SetSubmit(Main.bind(null, 2))
 	}
 	else if (key == 2) {
 		Output(`<p>An error has been introduced into the machine. Rather than an immediate and instantaneous reproduction, the output is populated five seconds after the input is processed, reproducing the exact and identical state of the input as it was processed five seconds ago.</p>
-		<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, and five seconds later another instance of you appears in the output, identical to what <i>du</i> was five seconds prior. What is the correct interpretation?</p>`);
+		<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, and five seconds later <b>another instance of you</b> appears in the output, identical to what <b><i>du</i></b> was five seconds prior. What is the correct interpretation?</p>`);
 		SetSubmit(Main.bind(null, 3));
 	}
 	else if (key == 3) {
-		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, as if you had travelled five seconds into the future, while another instance of you has continued to exist in the input. What is the correct interpretation?</p>`);
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, as if <b><i>du</i></b> had travelled five seconds into the future, while <b>another instance of you</b> has continued to exist in the input. What is the correct interpretation?</p>`);
 		SetSubmit(Main.bind(null, 4));
 	}
 }
