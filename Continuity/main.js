2166f5bbe714053fe7490e56e6a6f5c3c3581d81
KK̈ITAȺNIK <pu0awesomeness0re@gmail.com>
2024-01-31 02:17:29 -0800

    Revamp

--- Continuity/main.js
+++ Continuity/main.js
@@ -9,196 +9,48 @@ function Append(text) {
 	window.scrollTo(0, document.body.scrollHeight);
 }
 
-function AppendButtons(buttonList) {
-	for (let i = 0; i < buttonList.length; i++) {
-		Append(`<a onclick=\"FadeHistory();this.style.cursor = 'text';this.removeAttribute('onclick');` + buttonList[i][1] + `\">` + buttonList[i][0] + `</a>`);
-		if (i < buttonList.length - 1) {
-			Append(`<br/>`);
-		}
-	}
+function SetSubmit(myFunction) {
+	document.getElementById("submitButton").onclick = myFunction;
 }
 
-function FadeHistory() {
-	let contentChildren = document.getElementById("output").children;
-	for(let i = 0; i < contentChildren.length; i++) {
-		contentChildren[i].classList.add("faded");
-	}
+function SyncSliders(main, partner, mirror, partnerMirror) {
+	let mainVal = document.getElementById(main).value;
+	
+	document.getElementById(mirror).value = mainVal;
+	
+	if (!document.getElementById("sum100").checked) return;
+	
+	let partnerVal = document.getElementById(partner).value;
+	
+	if (partnerVal == 100 - mainVal) return;
+	
+	document.getElementById(partner).value = 100 - mainVal;
+	
+	document.getElementById(partnerMirror).value = 100 - mainVal;
 }
 
 function Main(key, param, subparam) {
-	let links = document.getElementsByTagName("a");
-	for(let i = 0; i < links.length; i++) {
-		if (links[i].style.cursor != "text") {
-			links[i].style.cursor = "text"
-			links[i].removeAttribute('onclick');
-			links[i].style.color = '#808080';
-		}
-	}
-	
-	
-	
+	document.getElementById("duSlider").value = 50;
+	document.getElementById("otherSlider").value = 50;
+	document.getElementById("duNumber").value = 50;
+	document.getElementById("otherNumber").value = 50;
 	if (key == 0) {
-		Output(`<p>Congratulations. You have invented a machine that allows for the instantaneous reproduction of absolutely anything. Whatever is inside its input enclosure will be seamlessly and immediately duplicated, appearing in an identical form in its output enclosure.</p>
-		<p>Through courage or stupidity, you have decided to begin a series of experiments in which you duplicate yourself.</p>
-		<p style="color:#808080">For the following experiments, the word <i>du</i> in second person, and <i>ich</i> in first person, will be used to refer to your (the player's) "experienced self": the instance of you from whom's perspective you are making these decisions. The term "the other" will refer to any instance of you that is not <i>du</i>. The choice of terminology here is arbitrary.</p>`);
-		
-		AppendButtons([
-			["Continue.", "Main(1, 'start')"]
-		]);
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, while another instance of you instantaneously appears in the output. What is the correct interpretation?</p>`);
+		document.getElementById("sliders").style.display = "block";
+		document.getElementById("submitButton").innerHTML = "Submit.";
+		SetSubmit(Main.bind(null, 1))
 	}
-	
-	
-	
 	else if (key == 1) {
-		if (param == "start") {
-			Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, while another instance of you instantaneously appears in the output. What is the correct interpretation?</p>`);
-			AppendButtons([
-				["<b><i>Ich</i></b> is entirely me.", "Main(1, 'other', 100)"],
-				["<b><i>Ich</i></b> is partly me.", "Main(1, 'other', 50)"],
-				["<b><i>Ich</i></b> is in no part me.", "Main(1, 'other', 0)"]
-			]);
-		}
-		else if (param == 'other') {
-			Append(`<p>And what about <b>the other</b> (the instance of you currently in the output)?</p>`);
-			answers.push([subparam]);
-			
-			if (subparam == 100) {
-				AppendButtons([
-					["<b>They</b> are also entirely me.", "Main(1, 'conclusion', 100)"],
-					["<b>They</b> are partly me; less than <i>ich</i>.", "Main(1, 'conclusion', 50)"],
-					["<b>They</b> are in no part me.", "Main(1, 'conclusion', 0)"]
-				]);
-			}
-			else if (subparam == 50) {
-				AppendButtons([
-					["<b>They</b> are entirely me.", "Main(1, 'conclusion', 100)"],
-					["<b>They</b> are partly me, but more than <i>ich</i>.", "Main(1, 'conclusion', 75)"],
-					["<b>They</b> are partly me; exactly as much as <i>ich</i>.", "Main(1, 'conclusion', 50)"],
-					["<b>They</b> are partly me, but less than <i>ich</i>.", "Main(1, 'conclusion', 25)"],
-					["<b>They</b> are in no part me.", "Main(1, 'conclusion', 0)"]
-				]);
-			}
-			else if (subparam == 0) {
-				AppendButtons([
-					["<b>They</b> are entirely me.", "Main(1, 'conclusion', 100)"],
-					["<b>They</b> are partly me; more than <i>ich</i>.", "Main(1, 'conclusion', 50)"],
-					["<b>They</b> are also in no part me.", "Main(1, 'conclusion', 0)"]
-				]);
-			}
-		}
-		
-		else if (param == 'conclusion') {
-			answers[0].push(subparam);
-			let duAssessment = {
-				100: "entirely",
-				50: "partially",
-				0: "in no part"
-			};
-			let otherAssessment = {
-				100: "entirely you",
-				75: "partially you, but more than <i>du</i>",
-				50: "partially you",
-				25: "partially you, but less than <i>du</i>",
-				0: "in no part you"
-			};
-			
-			let equality = "";
-			
-			if (answers[0][0] == answers[0][1]) {
-				equality = "equally as you as";
-			}
-			else if (answers[0][0] > answers[0][1]) {
-				equality = "more you than";
-			}
-			else if (answers[0][0] < answers[0][1]) {
-				equality = "less you than";
-			}
-			
-			Append("<p>In the case where <i>du</i> is in the input, <i>du</i> is " + duAssessment[answers[0][0]] + " you. The other is " + otherAssessment[answers[0][1]] + ". As a result, <i>du</i> in the input is " + equality + " the other.");
-			
-			AppendButtons([
-				["Continue.", "Main(2, 'start')"]
-			]);
-		}
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, while another instance of you continues to exist in the input. What is the correct interpretation?</p>`);
+		SetSubmit(Main.bind(null, 2))
 	}
-	
-	
-	
 	else if (key == 2) {
-		if (param == "start") {
-			Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, while another instance of you continues to exist in the input. What is the correct interpretation?</p>`);
-			AppendButtons([
-				["<b><i>Ich</i></b> is entirely me.", "Main(2, 'other', 100)"],
-				["<b><i>Ich</i></b> is partly me.", "Main(2, 'other', 50)"],
-				["<b><i>Ich</i></b> is in no part me.", "Main(2, 'other', 0)"]
-			]);
-		}
-		else if (param == 'other') {
-			Append(`<p>And what about <b>the other</b> (the instance of you currently in the input)?</p>`);
-			answers.push([subparam]);
-			
-			if (subparam == 100) {
-				AppendButtons([
-					["<b>They</b> are also entirely me.", "Main(2, 'conclusion', 100)"],
-					["<b>They</b> are partly me; less than <i>ich</i>.", "Main(2, 'conclusion', 50)"],
-					["<b>They</b> are in no part me.", "Main(2, 'conclusion', 0)"]
-				]);
-			}
-			else if (subparam == 50) {
-				AppendButtons([
-					["<b>They</b> are entirely me.", "Main(2, 'conclusion', 100)"],
-					["<b>They</b> are partly me, but more than <i>ich</i>.", "Main(2, 'conclusion', 75)"],
-					["<b>They</b> are partly me; exactly as much as <i>ich</i>.", "Main(2, 'conclusion', 50)"],
-					["<b>They</b> are partly me, but less than <i>ich</i>.", "Main(2, 'conclusion', 25)"],
-					["<b>They</b> are in no part me.", "Main(2, 'conclusion', 0)"]
-				]);
-			}
-			else if (subparam == 0) {
-				AppendButtons([
-					["<b>They</b> are entirely me.", "Main(2, 'conclusion', 100)"],
-					["<b>They</b> are partly me; more than <i>ich</i>.", "Main(2, 'conclusion', 50)"],
-					["<b>They</b> are also in no part me.", "Main(2, 'conclusion', 0)"]
-				]);
-			}
-		}
-		
-		else if (param == 'conclusion') {
-			answers[1].push(subparam);
-			let duAssessment = {
-				100: "entirely",
-				50: "partially",
-				0: "in no part"
-			};
-			let otherAssessment = {
-				100: "entirely you",
-				75: "partially you, but more than <i>du</i>",
-				50: "partially you",
-				25: "partially you, but less than <i>du</i>",
-				0: "in no part you"
-			};
-			
-			let equality = "";
-			
-			if (answers[1][0] == answers[1][1]) {
-				equality = "equally as you as";
-			}
-			else if (answers[1][0] > answers[1][1]) {
-				equality = "more you than";
-			}
-			else if (answers[1][0] < answers[1][1]) {
-				equality = "less you than";
-			}
-			
-			Append("<p>In the case where <i>du</i> is in the output, <i>du</i> is " + duAssessment[answers[1][0]] + " you. The other is " + otherAssessment[answers[1][1]] + ". As a result, <i>du</i> in the output is " + equality + " the other.");
-			
-			
-		}
+		Output(`<p>An error has been introduced into the machine. Rather than an immediate and instantaneous reproduction, the output is populated five seconds after the input is processed, reproducing the exact and identical state of the input as it was processed five seconds ago.</p>
+		<p>You enter the input and turn on the machine. <b><i>Du</i></b> remains in the input, and five seconds later another instance of you appears in the output, identical to what <i>du</i> was five seconds prior. What is the correct interpretation?</p>`);
+		SetSubmit(Main.bind(null, 3));
 	}
-	
-	
 	else if (key == 3) {
-		if (param == "start") {
-			Output
-		}
+		Output(`<p>You enter the input and turn on the machine. <b><i>Du</i></b> finds <b>themself</b> in the output, as if you had travelled five seconds into the future, while another instance of you has continued to exist in the input. What is the correct interpretation?</p>`);
+		SetSubmit(Main.bind(null, 4));
 	}
 }
